3072675a013c10c92da89c4069c95b1b29a5c0bf
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-31 23:40:51 +0200

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 const SimpleEditor = dynamic(() => import("react-simplemde-editor"), {
 	ssr: false,
+	loading: () => <p>Loading...</p>,
 });
 import { useForm, Controller } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 
 import { useRouter } from "next/navigation";
 
@@ -17,26 +18,37 @@ interface CreateIssueProps {
 }
 
 export default function NewIssuePage() {
+	const [error, setError] = useState("");
 	const router = useRouter();
 	const { register, handleSubmit, control } = useForm<CreateIssueProps>();
 	return (
-		<form
-			onSubmit={handleSubmit(async (data) => {
-				const result = await axios.post("/api/issues", data);
-				console.log(result);
-				router.push("/issues");
-			})}
-			className="space-y-3 max-w-xl"
-		>
-			<TextField.Root placeholder="Title" {...register("title")} />
-			<Controller
-				control={control}
-				name="description"
-				render={({ field }) => (
-					<SimpleEditor placeholder="Description" {...field} />
-				)}
-			/>
-			<Button type="submit">Submit New Issue</Button>
-		</form>
+		<div className="max-w-xl">
+			{error && (
+				<Callout.Root color="red" className="mb-4">
+					<Callout.Text>{error}</Callout.Text>
+				</Callout.Root>
+			)}
+			<form
+				onSubmit={handleSubmit(async (data) => {
+					try {
+						await axios.post("/api/issues", data);
+						router.push("/issues");
+					} catch (error) {
+						setError("An unexpected error occured");
+					}
+				})}
+				className="space-y-3 "
+			>
+				<TextField.Root placeholder="Title" {...register("title")} />
+				<Controller
+					control={control}
+					name="description"
+					render={({ field }) => (
+						<SimpleEditor placeholder="Description" {...field} />
+					)}
+				/>
+				<Button type="submit">Submit New Issue</Button>
+			</form>
+		</div>
 	);
 }
