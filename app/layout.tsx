bfaaba86a82af9cbd8a7bfbe352f88d2ca15e915
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-31 18:03:32 +0200

    customize radix ui and inter font working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import type { Metadata } from "next";
-
-import { Inter } from "next/font/google";
-import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
+import type { Metadata } from "next";
 import Navbar from "./Navbar";
+import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -20,10 +23,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
-				<Theme>
+			<body className={inter.variable}>
+				<Theme accentColor="violet" panelBackground="solid" radius="large">
 					<Navbar />
-					<main className=" p-5">{children}</main>
+					<main className="p-5">{children}</main>
 				</Theme>
 			</body>
 		</html>
