8f2a538a9ed4510486f1ba45240744d679019ba6
Karan Chauhan <karanchouhan211@gmail.com>
2024-06-01 15:11:11 +0200

    showing issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "./Navbar";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -22,11 +22,13 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className={inter.variable}>
 				<Theme accentColor="violet" panelBackground="solid" radius="large">
 					<Navbar />
-					<main className="p-5">{children}</main>
+					<main className="p-5">
+						<Container>{children}</Container>
+					</main>
 				</Theme>
 			</body>
 		</html>
