46bcb559bb07abd3326efa2326ca7bb71d2e3a8f
Karan Chauhan <karanchouhan211@gmail.com>
2024-06-01 21:30:14 +0200

    Adding Edit button

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,18 +1,23 @@
-import { Box, Flex, Heading } from "@radix-ui/themes";
+import { Box, Flex, Grid, Heading } from "@radix-ui/themes";
 import { Skeleton } from "@/app/components";
 import "react-loading-skeleton/dist/skeleton.css";
 export default function loading() {
 	return (
-		<Box className="max-w-xl">
-			<Heading>
-				<Skeleton />
-			</Heading>
-			<Flex gap={"3"} my={"3"}>
-				<Skeleton width={"4rem"} />
-				<Skeleton width={"4rem"} />
-			</Flex>
+		<Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+			<Box className="max-w-xl">
+				<Heading>
+					<Skeleton />
+				</Heading>
+				<Flex gap={"3"} my={"3"}>
+					<Skeleton width={"4rem"} />
+					<Skeleton width={"4rem"} />
+				</Flex>
 
-			<Skeleton height={"25rem"} />
-		</Box>
+				<Skeleton height={"25rem"} />
+			</Box>
+			<Box>
+				<Skeleton width={"5rem"} height={"2rem"} />
+			</Box>
+		</Grid>
 	);
 }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 interface Props {
 	params: { id: string };
 }
@@ -15,15 +18,23 @@ export default async function page({ params }: Props) {
 	if (!issue) notFound();
 	await delay(2000);
 	return (
-		<div>
-			<Heading>{issue.title}</Heading>
-			<Flex gap={"3"} my={"3"}>
-				<IssueStatusBadge status={issue.status} />
-				<p>{issue.createdAt.toDateString()}</p>
-			</Flex>
-			<Card className="prose mt-6">
-				<ReactMarkdown>{issue.description}</ReactMarkdown>
-			</Card>
-		</div>
+		<Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+			<Box>
+				<Heading>{issue.title}</Heading>
+				<Flex gap={"3"} my={"3"}>
+					<IssueStatusBadge status={issue.status} />
+					<p>{issue.createdAt.toDateString()}</p>
+				</Flex>
+				<Card className="prose mt-6">
+					<ReactMarkdown>{issue.description}</ReactMarkdown>
+				</Card>
+			</Box>
+			<Box>
+				<Button>
+					<Pencil2Icon />
+					<Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+				</Button>
+			</Box>
+		</Grid>
 	);
 }
