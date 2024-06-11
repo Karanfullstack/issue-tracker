9135bec3342d009b8161af0d0de9c82b416eca70
Karan Chauhan <karanchouhan211@gmail.com>
2024-06-11 17:35:52 +0200

    sorting issues

--- app/issues/lists/page.tsx
+++ app/issues/lists/page.tsx
@@ -6,7 +6,6 @@ import { Issue, Status } from "@prisma/client";
 import { Link } from "@/app/components";
 import NextLink from "next/link";
 import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
-import { it } from "node:test";
 
 interface SearchParamsProps {
 	searchParams: {
@@ -17,19 +16,6 @@ interface SearchParamsProps {
 }
 
 export default async function IssuePage({ searchParams }: SearchParamsProps) {
-	const status = Object.values(Status).includes(searchParams.status)
-		? searchParams.status
-		: undefined;
-
-	const issues = await prisma.issue.findMany({
-		where: {
-			status,
-		},
-		orderBy: {
-			[searchParams.orderBy]: searchParams.sortType,
-		},
-	});
-
 	const columns: { label: string; value: keyof Issue; classNames?: string }[] =
 		[
 			{ label: "Issue", value: "title" },
@@ -41,7 +27,29 @@ export default async function IssuePage({ searchParams }: SearchParamsProps) {
 			},
 		];
 
-	console.log({ ...searchParams, orderBy: "title", sortType: "asc" });
+	const status = Object.values(Status).includes(searchParams.status)
+		? searchParams.status
+		: undefined;
+
+	const orderBy = columns
+		.map((column) => column.value)
+		.includes(searchParams.orderBy)
+		? {
+				[searchParams.orderBy]:
+					searchParams.sortType === "asc"
+						? "asc"
+						: searchParams.sortType === "desc"
+						? "desc"
+						: undefined,
+		  }
+		: undefined;
+
+	const issues = await prisma.issue.findMany({
+		where: {
+			status,
+		},
+		orderBy,
+	});
 
 	return (
 		<div>
@@ -101,9 +109,9 @@ interface ArrowCompoentI {
 const ArrowCompoent = ({ value, searchParams }: ArrowCompoentI) => {
 	return value === searchParams.orderBy ? (
 		searchParams.sortType === "asc" ? (
-			<ArrowUpIcon  className="inline ml-2"/>
+			<ArrowUpIcon className="inline ml-2" />
 		) : (
-			<ArrowDownIcon  className="inline ml-2"/>
+			<ArrowDownIcon className="inline ml-2" />
 		)
 	) : null;
 };
