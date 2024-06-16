75d4c5dd252e8ff5a998340232f1ea0cd6c7bebf
Karan Chauhan <karanchouhan211@gmail.com>
2024-06-16 21:08:54 +0200

    Building issue summary component

--- app/LatestIssue.tsx
+++ app/LatestIssue.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/prisma/client";
-import { Avatar, Flex, Table } from "@radix-ui/themes";
+import { Avatar, Flex, Heading, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 import { IssueStatusBadge } from "./components";
@@ -15,29 +15,32 @@ export default async function LatestIssue() {
 		},
 	});
 	return (
-		<Table.Root>
-			<Table.Body>
-				{issues.map((issue) => (
-					<Table.Row key={issue.id}>
-						<Table.Cell>
-							<Flex align={"start"} justify={"between"}>
-								<Flex direction="column" align={"start"} gap={"2"}>
-									<Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-									<IssueStatusBadge status={issue.status} />
+		<>
+			<Heading size={"6"}>Latest Issues</Heading>
+			<Table.Root>
+				<Table.Body>
+					{issues.map((issue) => (
+						<Table.Row key={issue.id}>
+							<Table.Cell>
+								<Flex align={"start"} justify={"between"}>
+									<Flex direction="column" align={"start"} gap={"2"}>
+										<Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+										<IssueStatusBadge status={issue.status} />
+									</Flex>
+									{issue.assignedToUser && (
+										<Avatar
+											size={"2"}
+											radius="full"
+											src={issue.assignedToUser?.image!}
+											fallback="?"
+										/>
+									)}
 								</Flex>
-								{issue.assignedToUser && (
-									<Avatar
-										size={"2"}
-										radius="full"
-										src={issue.assignedToUser?.image!}
-										fallback="?"
-									/>
-								)}
-							</Flex>
-						</Table.Cell>
-					</Table.Row>
-				))}
-			</Table.Body>
-		</Table.Root>
+							</Table.Cell>
+						</Table.Row>
+					))}
+				</Table.Body>
+			</Table.Root>
+		</>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import prisma from "@/prisma/client";
+import IssueSumary from "./IssueSumary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home() {
+export default async function Home() {
+	const inProgress = await prisma.issue.count({
+		where: { status: "IN_PROGRESS" },
+	});
+	const open = await prisma.issue.count({
+		where: { status: "OPEN" },
+	});
+	const closed = await prisma.issue.count({
+		where: { status: "CLOSED" },
+	});
 	return (
 		<main>
-			<LatestIssue />
+			<IssueSumary inProgress={inProgress} closed={closed}  open={open}/>
 		</main>
 	);
 }
